e4cde014a37a63606c3bfe0f725ac8a89552e21d
ormigar <https://github.com/kof467/text.git>
2019-12-10 11:35:55 +0800

    hehe

---
+++ app/src/main/java/com/example/ormigar/xiangmu/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.ormigar.xiangmu;
+
+import android.content.Intent;
+
+import android.support.v7.app.AppCompatActivity;
+
+
+
+import android.os.Bundle;
+
+import android.util.Log;
+
+import android.view.View;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.NetworkInterface;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+//登陆界面
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
